60dbc1ab0c39adc53ac89a08075bc042284dc898
MritachiUchiha <gamerpubg1008@gmail.com>
2024-01-23 23:16:04 +0100

    shutdown.bat
    inside the bat file i example the basic of it but there more commands and you can experiment

---
+++ shutdown.bat
@@ -0,0 +1,7 @@
+# its pretty easy to understand with this bat file you can just shutdown
+# the /s stands for shutdown
+# the /t stands for time but in seconds counted
+# for example if you want to restart your pc you will just type: shutdown.exe /r /t 120
+# there more commands but this are the basics of it :)
+# also to cancel the reboot or shutdown just type: shutdown.exe /a
+shutdown.exe /s /t 120
