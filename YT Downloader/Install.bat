3abf25b4a92ab266f89531de487cea1d74fe6b5e
MritachiUchiha <gamerpubg1008@gmail.com>
2024-10-24 15:38:28 +0200

    test

--- YT Downloader/Install.bat
+++ YT Downloader/Install.bat
@@ -1,10 +1,16 @@
 @echo off
 echo "Press Any Key"
 call ./read-me.bat
-echo Download will start 3,2,1,Now!!
-powershell.exe ./Packages/install-scoop.ps1
-powershell.exe ./Packages/Install-node.js.ps1
-scoop update &nodejs&scoop update python&scoop update&npm i
+echo Download will start 3, 2, 1, Now!!
+
+rem Open PowerShell and run the scripts
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "./Packages/install-scoop.ps1"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File "./Packages/Install-node.js.ps1"
+
+rem Update scoop and install packages
+powershell.exe -ExecutionPolicy Bypass -Command "scoop update; scoop install nodejs; scoop update python"
+
+rem Install npm packages
+powershell.exe -ExecutionPolicy Bypass -Command "npm install"
 
 PAUSE
-
